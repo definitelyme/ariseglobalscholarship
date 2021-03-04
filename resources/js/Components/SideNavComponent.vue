4d73247ed28817489fdb837b58c4f934cce156a6
Brendan E <ejike.br@gmail.com>
2021-03-04 18:28:50 +0100

    Add divider to sidebar

--- resources/js/Components/SideNavComponent.vue
+++ resources/js/Components/SideNavComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="responsive-menu">
-    <ul>
+    <ul class="side-nav-links">
       <li>
         <a :href="route('/')" title="" :active="route().current('/')">Home</a>
       </li>
@@ -50,21 +50,69 @@
         >
       </li>
 
-      <li>
-        <nav-link-component :href="route('logout')" method="post"
-          >Logout
-        </nav-link-component>
-      </li>
+      <ul>
+        <li>
+          <nav-link-component
+            :href="route('login')"
+            v-if="!user"
+            replace
+            preserve-scroll
+            preserve-state
+            >Login
+          </nav-link-component>
+        </li>
+
+        <li>
+          <nav-link-component
+            :href="route('register')"
+            v-if="!user"
+            replace
+            preserve-scroll
+            preserve-state
+            >Register
+          </nav-link-component>
+        </li>
+
+        <li>
+          <nav-link-component
+            :href="route('logout')"
+            method="post"
+            v-if="user"
+            replace
+            >Logout
+          </nav-link-component>
+        </li>
+      </ul>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  inject: ["user"],
   data() {
     return {
       //
     };
   },
+
+  mounted() {
+    // This adds a divider between the second to last element and the Auth links
+    let divider = document.createElement("hr");
+    divider.className = "rounded";
+    document
+      .querySelector("ul.side-nav-links > li:nth-last-child(2)")
+      .appendChild(divider);
+  },
 };
 </script>
+
+<style>
+ul.side-nav-links > li:nth-last-child(2) {
+  margin-bottom: 0px !important;
+}
+
+hr.rounded {
+  border-top: 2px solid #eee;
+}
+</style>
