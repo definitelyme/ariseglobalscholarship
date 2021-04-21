30f3a4d2fd7e0d7599b1dd8b9ce64665840bab72
Brendan Ejike <ejike.br@gmail.com>
2021-04-21 16:45:09 +0100

    Improve performance

--- resources/js/Components/TopNavComponent.vue
+++ resources/js/Components/TopNavComponent.vue
@@ -22,6 +22,16 @@
           <li>
             <a :href="route('scholarship.apply')" title="">Apply</a>
           </li>
+          <!-- <li>
+            <a :href="route('scholarship.about', settings.version)" title=""
+              >Eligibility</a
+            >
+          </li>
+          <li>
+            <a :href="route('scholarship.about', settings.version)" title=""
+              >Requirements</a
+            >
+          </li> -->
         </ul>
       </li>
 
@@ -45,7 +55,7 @@
 
       <li>
         <a
-          :href="route('about', settings.version)"
+          :href="route('about')"
           title=""
           :class="{ active: route().current().includes('about') }"
           >About</a
