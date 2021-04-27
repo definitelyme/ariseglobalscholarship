9ae321f0f0c69795e6ca67ad8382b82e3dd648fa
Brendan Ejike <ejike.br@gmail.com>
2021-04-27 15:31:10 +0100

    Remove localStorage from covid-banner & Add new courses

--- resources/js/Components/CovidBottomBanner.vue
+++ resources/js/Components/CovidBottomBanner.vue
@@ -16,16 +16,17 @@ export default {
 
   data() {
     return {
-      show:
-        JSON.parse(localStorage?.showCovidBanner ?? null) ??
-        true & this.settings.showCovidBanners & this.settings.showBottomBanner,
+    //   show:
+    //     JSON.parse(localStorage?.showCovidBanner ?? null) ??
+    //     true & this.settings.showCovidBanners & this.settings.showBottomBanner,
+        show: this.settings.showCovidBanners & this.settings.showBottomBanner,
     };
   },
 
   methods: {
     dismiss() {
       this.show = false;
-      localStorage.showCovidBanner = false;
+    //   localStorage.showCovidBanner = false;
     },
   },
 };
